537b71d5fe327eab02951e2e201f5db902a80f64
thalha-dev <dev.thalha@gmail.com>
2024-02-29 15:25:03 +0530

    add controller for emp_details and create an endpoing to add new employee

---
+++ src/main/java/com/project/talentSearch/controllers/EmpDetailsController.java
@@ -0,0 +1,68 @@
+package com.project.talentSearch.controllers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.talentSearch.dto.EmpDetailDto;
+import com.project.talentSearch.entity.EmpDetails;
+import com.project.talentSearch.entity.EmployeeExperience;
+import com.project.talentSearch.entity.Roles;
+import com.project.talentSearch.repositories.EmpDetailsRepository;
+import com.project.talentSearch.repositories.RolesRepository;
+
+@RestController
+public class EmpDetailsController {
+
+  @Autowired
+  private ModelMapper modelMapper;
+
+  @Autowired
+  private EmpDetailsRepository empDetailsRepository;
+
+  @Autowired
+  private RolesRepository rolesRepository;
+
+  @PostMapping("/employee/addNewEmployee")
+  public ResponseEntity<EmpDetailDto> addNewEmployee(@RequestBody EmpDetails empForm) {
+    if (empForm != null) {
+      // Find or create the role
+      Roles role = rolesRepository.findByRoleName(empForm.getRole().getRoleName());
+      if (role == null) {
+        role = new Roles();
+        role.setRoleName(empForm.getRole().getRoleName());
+        role = rolesRepository.save(role);
+      }
+
+      // Create the new employee
+      EmpDetails newEmployee = EmpDetails.builder()
+          .empName(empForm.getEmpName())
+          .empEmail(empForm.getEmpEmail())
+          .empPhone(empForm.getEmpPhone())
+          .empAddress(empForm.getEmpAddress())
+          .empJoiningDate(empForm.getEmpJoiningDate())
+          .role(role)
+          .build();
+
+      // Associate the employee with the experiences
+      if (empForm.getExperiences() != null) {
+        for (EmployeeExperience experience : empForm.getExperiences()) {
+          experience.setEmployee(newEmployee);
+        }
+        newEmployee.setExperiences(empForm.getExperiences());
+      }
+
+      newEmployee = empDetailsRepository.save(newEmployee);
+      EmpDetailDto dto = modelMapper.map(newEmployee, EmpDetailDto.class);
+
+      return new ResponseEntity<EmpDetailDto>(dto, HttpStatus.CREATED);
+    } else {
+      return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+}
