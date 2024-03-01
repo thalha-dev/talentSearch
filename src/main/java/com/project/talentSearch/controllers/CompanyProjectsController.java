5caa784904335cc7984fd022999b2d2d0ff9d737
thalha-dev <dev.thalha@gmail.com>
2024-03-01 15:39:11 +0530

    add controller for CompanyProjects entity and create an endpoing to add new project

---
+++ src/main/java/com/project/talentSearch/controllers/CompanyProjectsController.java
@@ -0,0 +1,30 @@
+package com.project.talentSearch.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.talentSearch.entity.CompanyProjects;
+import com.project.talentSearch.repositories.CompanyProjectsRepository;
+
+@RestController
+public class CompanyProjectsController {
+
+  @Autowired
+  private CompanyProjectsRepository companyProjectsRepository;
+
+  @PostMapping("projects/addNewProject")
+  public ResponseEntity<CompanyProjects> addNewProject(@RequestBody CompanyProjects projectForm) {
+    if (projectForm != null && projectForm.getProjectName() != null && projectForm.getProjectStartDate() != null
+        && projectForm.getProjectDuetDate() != null && projectForm.getProjectStatus() != null) {
+      CompanyProjects newProject = companyProjectsRepository.save(projectForm);
+      return new ResponseEntity<>(newProject, HttpStatus.CREATED);
+    } else {
+      return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+}
--- src/main/java/com/project/talentSearch/entity/CompanyProjects.java
+++ src/main/java/com/project/talentSearch/entity/CompanyProjects.java
@@ -37,6 +37,16 @@ public class CompanyProjects {
   @Column(name = "project_due_date")
   private Date projectDuetDate;
 
+  // New: The project is in the early stages of development or planning.
+  // Open: The project has been started but is not yet completed.
+  // InProgress: Work is currently being done on the project.
+  // Completed: All work on the project has been finished.
+  // Upcoming: The project is scheduled to be started in the near future.
+  // Pending: The project has begun but has not yet been completed and is awaiting
+  // -further action or approval.
+  // Overdue: The project has passed its completion date and is behind schedule.
+  // OnHold: Work on the project has been temporarily stopped.
+  // Canceled: The project ended before completion.
   @Column(name = "project_status")
   private String projectStatus;
 
