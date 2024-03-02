1776b78679141d2ce17054f379627f94f39d4b02
thalha-dev <dev.thalha@gmail.com>
2024-03-02 08:06:52 +0530

    update the current status type in dto

--- src/main/java/com/project/talentSearch/dto/EmployeeProjectMapDto.java
+++ src/main/java/com/project/talentSearch/dto/EmployeeProjectMapDto.java
@@ -14,16 +14,9 @@ import lombok.NoArgsConstructor;
 public class EmployeeProjectMapDto {
   private Long Id;
 
-  /*
-   * Assigned: The employee has been assigned to the project but has not started
-   * work yet.
-   * Active: The employee is currently working on the project.
-   * OnHold: Work by the employee on the project has been temporarily paused.
-   * Completed: The employee has finished all their tasks for the project.
-   * Released: The employee was part of the project but has now been released from
-   * it.
-   */
-  private String currentStatus;
+  // 0: Not occupied
+  // 1 : occupied
+  private Integer currentStatus;
 
   private Integer occupancy;
 
